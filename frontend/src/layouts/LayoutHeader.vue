44173ec4d94af587bf98d39d382cfacdd0907928
Leon <napoleon.lee0114@gmail.com>
2026-02-08 19:16:34 +0800

    fix (LayoutHeader): change web logo pic to v2

--- frontend/src/layouts/LayoutHeader.vue
+++ frontend/src/layouts/LayoutHeader.vue
@@ -8,7 +8,7 @@
             @click="closeMobileMenu"
             class="flex items-center"
           >
-            <img src="/logo-text.png" alt="LiveFit+" class="h-6 w-auto" />
+            <img src="/logo-text-v2.png" alt="LiveFit+" class="h-6 w-auto" />
             <span class="sr-only">LiveFit+</span>
           </router-link>
         </h1>
@@ -181,7 +181,7 @@
             </li>
             <li v-if="name">
               <button
-                @click="closeMobileMenu(), logout()"
+                @click="(closeMobileMenu(), logout())"
                 class="inline-block py-2 text-neutral-300 hover:text-primary-0 transition-all font-medium text-base leading-[150%] w-full text-left"
               >
                 登出
